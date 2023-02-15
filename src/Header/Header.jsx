28843803a0c481859cfaac3320a2f854848d5f6e
AnnaMintel <annikamintel99@yandex.ru>
2023-02-15 15:59:56 +0300

    feat: added header

---
+++ src/Header/Header.jsx
@@ -0,0 +1,12 @@
+import s from './Header.module.css';
+import ghImage from "./../img/gh.svg";
+
+export const Header = () => {
+  return (
+    <div className={s.header}>
+      <img className={s.ghImg} src={ghImage} alt='GH-img' />
+      <input className={s.headerInput} type="search" placeholder="Enter username" />
+    </div>
+  );
+}
+
